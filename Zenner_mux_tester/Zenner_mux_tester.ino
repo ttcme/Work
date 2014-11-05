2b99c923a7b7580b313ab5ff0614a0c3d1ba8115
ttcme <c.m.evans-08@student.lboro.ac.uk>
2014-11-05 10:22:01 +0000

    added current changing

--- Zenner_mux_tester/Zenner_mux_tester.ino
+++ Zenner_mux_tester/Zenner_mux_tester.ino
@@ -1,12 +1,17 @@
 //define included libraries
 #include <SD.h>
 #include <SPI.h>
-
+//SPI Pins
+#define SCK 13
+#define MISO 12
+#define MOSI 11
+#define SS 10
 //slave select pins
 #define ADC_SS 2
 #define MUX_SS 3
 #define SD_SS 4
 //define input pins
+#define STOP_PIN 7
 #define N_DRDY 8
 #define TEMP_PIN A0
 // define output pins
@@ -21,7 +26,9 @@
 #define ADC 1
 #define MUX 2
 #define SDCARD 3
-
+#define NOOFZENER 8
+File datalog;
+long sample_period = 10000; //complete sample set 10 seconds after last sample completion
 
 void Set_Current(int current){ //current in mA
   current = current/4;          // 0-1000 mA is approx scaled to 0-255, actually to 250
@@ -73,36 +80,119 @@ int Read_Temp(){    //reads temperature and maps to degrees C
   
 
 void setup() {
-pinMode(ADC_SS,OUTPUT); 
-pinMode(MUX_SS,OUTPUT);
-pinMode(SD_SS,OUTPUT);
-pinMode(N_DRDY,INPUT);
-
-digitalWrite(ADC_SS,HIGH);
-digitalWrite(MUX_SS,HIGH);
-digitalWrite(SD_SS,HIGH);
-Set_Current(0);
-//SPI setup
-SPI.begin();
-SPI.setBitOrder(MSBFIRST); //correct
-SPI.setDataMode(SPI_MODE3); //i think correct, unsure
-
-SD.begin(SD_SS);
-SD.open("Datalogger.csv", FILE_WRITE);
-
-
-//adc setup
-digitalWrite(ADC_SS,LOW);
-SPI.transfer(0x10);
-SPI.transfer(0x68);
-digitalWrite(ADC_SS,HIGH);
-//Deselect a mux channel
-Mux_Channel(0);
-
+  pinMode(ADC_SS,OUTPUT); 
+  pinMode(MUX_SS,OUTPUT);
+  pinMode(SD_SS,OUTPUT);
+  pinMode(N_DRDY,INPUT);
+  pinMode(SS, OUTPUT); //MUST BE THIS WAT TO ALLOW FOR SPI, EVEN IF NOT USED
+  digitalWrite(ADC_SS,HIGH);
+  digitalWrite(MUX_SS,HIGH);
+  digitalWrite(SD_SS,HIGH);
+  Select(NONE);
+  Set_Current(0);
+  //SPI setup
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST); //correct
+  SPI.setDataMode(SPI_MODE3); //i think correct, unsure
+  
+  SD.begin(SD_SS); //select not needed, all is set up inside begin. Is needed in future
+  datalog = SD.open("Datalog1.csv", FILE_WRITE);
+  //File header Writing, CSV format
+  datalog.print(" ,");
+  for (int x = 1; x <= NOOFZENER; x++){
+    datalog.print("Zener");
+    datalog.print(x);
+    if(x == NOOFZENER){
+      datalog.println(", ");
+    }
+    else{
+      datalog.print(", ,");
+    }
+  }
+  datalog.print("Temperature,");
+  for (int x = 1; x<= NOOFZENER; x++){
+    if (x < NOOFZENER){
+      datalog.print("Current,Voltage,");
+    }
+    else{
+      datalog.println("Current,Voltage");
+    }
+  }
+  //adc setup
+  Select(ADC);
+  SPI.transfer(0x10);
+  SPI.transfer(0x68);
+  Select(NONE);
+  //Deselect a mux channel
+  Mux_Channel(0);
 }
 
 void loop() {
   
-  
+  // loop variables
+  long current = 0;
+  int holder = 0; //used for transient storage of higher or lower byte from adc
+  long voltage = 0;
+  delay(sample_period);
+  const unsigned int CURRENTLOOKUP[] = {0 , 1000, 4, 500, 8, 200, 20, 100, 48}; // alternation high and low current to prevent extended periods of heating
+  datalog.print(Read_Temp());
+  datalog.print(",");
+  //asumed order of mux connections. 1:zener1 current
+                            //      2:       voltage
+                           //       3:zener2 current
+                            //      4:       voltage etc..
+  for (int incursel = 0 ; incursel <=8;incursel++){
+    Set_Current(CURRENTLOOKUP[incursel]);
+    delay(1);
+    for (int x=1; x<=8; x++){
+      int y =2*x+1;
+      Mux_Channel(y);
+      Select(ADC);
+      while (N_DRDY){
+        delay(1);}
+        
+      SPI.transfer(0x38); //read request
+      holder = SPI.transfer(0xFF); //upper adc byte to lower byte inholder
+      current = holder << 8; //lower byte to upper byte in current
+      holder = SPI.transfer(0xFF); //lower adc byte to lower holder
+      current =+ holder; //add lower to upper current
+      
+      y++;
+      Mux_Channel(y);
+      Select(ADC);
+      while (N_DRDY){
+        delay(1);}
+      
+      SPI.transfer(0x38); //read request
+      holder = SPI.transfer(0xFF); //upper adc byte to lower byte inholder
+      voltage = holder << 8; //lower byte to upper byte in voltage
+      holder = SPI.transfer(0xFF); //lower adc byte to lower holder
+      voltage =+ holder; //add lower to upper voltage
+      
+      Select(SDCARD); // select sdcard for datawriting
+      datalog.print(current);
+      datalog.print(",");
+      datalog.print(voltage);
+      if (x == 8){ // if last in loop start new line
+        datalog.println();
+        if (digitalRead(STOP_PIN) == HIGH){ //exit clause at the end of a cycle if pin high
+          Select(NONE);
+          while(true){//double blink to indicate stopped, SD card safe to remove
+            digitalWrite(SCK, HIGH);
+            delay(500);
+            digitalWrite(SCK, LOW);
+            delay(500);
+            digitalWrite(SCK, HIGH);
+            delay(500);
+            digitalWrite(SCK, LOW);
+            delay(1000);
+          }
+        }
+      }
+      else {
+        datalog.print(",");
+      }
+    }
+  }
 }
 
